2f8f6c6533d20a80e75ba3fc016e016972e50fe6
brilord <143294125+hellobrian1@users.noreply.github.com>
2024-03-28 16:11:15 -0400

    committing

--- services/userCred.js
+++ services/userCred.js
@@ -5,67 +5,70 @@ import bcrypt from "bcrypt";
 
 config(); //configure
 
-const userCredRouter = express.Router()
+const userCredRouter = express.Router();
 
-userCredRouter.use(express.json())
+userCredRouter.use(express.json());
 
-const dbName = 'Deliver-IT';
+const dbName = "Deliver-IT";
 const client = new MongoClient(process.env.DB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 // Route to handle user login
-userCredRouter.post('/Login', async (req, res) => {
+userCredRouter.post("/Login", async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).send('Request body must contain email and password');
+    return res.status(400).send("Request body must contain email and password");
   }
   try {
     const db = client.db(dbName);
-    const usersCollection = db.collection('users');
-    
+    const usersCollection = db.collection("users");
 
     //console.log(hello);
     // Find the user by email
     const user = await usersCollection.findOne({ email });
     if (!user) {
-      return res.status(404).send('User not found');
+      return res.status(404).send("User not found");
     }
 
     // Check if the password is correct
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).send('Password is incorrect');
+      return res.status(401).send("Password is incorrect");
     }
 
     // User is authenticated
-    res.status(200).json({ message: "User Login Successful" });
+    res.status(200).json({
+      message: "User Login Successful",
+      userInfo: JSON.stringify(user),
+    });
   } catch (error) {
-    console.error('Login error:', error);
-    res.status(500).send('An error occurred during login');
+    console.error("Login error:", error);
+    res.status(500).send("An error occurred during login");
   }
   // No need to close the MongoDB client after every request
 }); //
 // Route to handle user registration
-userCredRouter.post('/register', async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
+userCredRouter.post("/register", async (req, res) => {
+  const { firstName, lastName, email, password, role } = req.body;
 
   // Simple validation
-  if (!firstName || !lastName || !email || !password) {
-    return res.status(400).send('Please enter all fields');
+  if (!firstName || !lastName || !email || !password || !role) {
+    return res.status(400).send("Please enter all fields");
   }
 
   try {
     const db = client.db(dbName);
-    const usersCollection = db.collection('users');
+    const usersCollection = db.collection("users");
 
     // Check if the user already exists
     const existingUser = await usersCollection.findOne({ email });
     if (existingUser) {
-      return res.status(400).send('User already exists');
+      return res.status(400).send("User already exists");
     }
 
     // Hash password
@@ -77,7 +80,9 @@ userCredRouter.post('/register', async (req, res) => {
       firstName,
       lastName,
       email,
-      password: hashedPassword
+      password: hashedPassword,
+      role,
+      
     };
 
     // Insert the new user into the database
@@ -85,10 +90,24 @@ userCredRouter.post('/register', async (req, res) => {
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    console.error('Registration error:', error);
-    res.status(500).send('An error occurred during registration');
+    console.error("Registration error:", error);
+    res.status(500).send("An error occurred during registration");
   }
 });
 
-export {userCredRouter}
+userCredRouter.post("/userDetails", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const db = client.db(dbName);
+    const usersCollection = db.collection("users");
+    const user = await usersCollection.findOne({ email });
+    res.status(200).json({
+      userInfo: JSON.stringify(user),
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).send("An error occurred during login");
+  }
+});
 
+export { userCredRouter };
