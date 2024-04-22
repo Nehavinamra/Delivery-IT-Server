4c139f68827ebd3ce8dfe2a8fd55899437c93288
Nehavinamra <vinamraneha@gmail.com>
2024-04-22 19:31:13 -0400

    adding chat server

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
-import http from "http";
+import { createServer } from "http";
 import { userCredRouter } from "./services/userCred.js";
 import searchRouter from "./services/Search.js";
 import { config } from "dotenv";
 import { paymentRouter } from "./services/payment.js";
 import employeeRouter from "./services/Search.js";
-import { Server as SocketIO } from "socket.io";
-import { chatRouter } from "./services/Chat.js";
+import { Server } from "socket.io";
 
 config();
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
 const PORT = process.env.PORT || 8080;
-const io = new SocketIO(server, {
+const chatPort = 7000;
+
+const newChatServer = createServer();
+const chatServer = new Server(newChatServer, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true,
   },
 });
-// MongoDB Connection URL
-const mongoURI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://nehaV:FV47KppuT6qWxcFz@cluster0.00tx6ph.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connection established successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
-mongoose
-  .connect(process.env.DB_URI)
-  .then(() => console.log("MongoDB connection established successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(
   cors({
@@ -46,13 +28,28 @@ app.use(
   })
 );
 
+chatServer.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  //Listens and logs the message to the console
+  socket.on("message", (data) => {
+    chatServer.emit("messageResponse", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+});
 app.use(express.json());
 app.use(userCredRouter);
 app.use(paymentRouter);
 app.use(employeeRouter);
 app.use("/api/search", searchRouter);
-app.use(chatRouter);
 
 server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
+});
+
+newChatServer.listen(chatPort, () => {
+  console.log(`Server listening on ${chatPort}`);
 });
